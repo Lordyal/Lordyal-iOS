85ec9f17c0ac8f90bb7b6150ae0318ed0178c13e
Sidney Sadel <sidneysadel@gmail.com>
2023-04-05 16:31:08 -0400

    Initial Commit

---
+++ Lordyal/LordyalApp.swift
@@ -0,0 +1,17 @@
+//
+//  LordyalApp.swift
+//  Lordyal
+//
+//  Created by Sidney Sadel on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct LordyalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
