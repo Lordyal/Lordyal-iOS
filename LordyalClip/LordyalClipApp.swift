38b18920528fbfbfd32d61c7a926743b36726d50
Sidney Sadel <sidneysadel@gmail.com>
2023-04-06 11:17:29 -0400

    add app clip

---
+++ LordyalClip/LordyalClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  LordyalClipApp.swift
+//  LordyalClip
+//
+//  Created by Sidney Sadel on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct LordyalClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
